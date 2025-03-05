2a1a28c5cf14b21283b0a56ba9b967f3314b41a5
Shenghua He <shenghuahe@mbp-sh.local>
2025-03-05 00:17:36 -0800

    modified:   scripts/data_preprocessing.sh

--- scripts/data_preprocessing.sh
+++ scripts/data_preprocessing.sh
@@ -1,6 +1,6 @@
 DATA_PATH=datasets/raw_datasets/
 OUTPUT_DIR=datasets/formatted_datasets/
-DATASET_ID=hhrlhf_helpful   # summarize, hhrlhf_helpful
+DATASET_ID=summarize   # summarize, hhrlhf_helpful
 python datasets/data_preprocessing.py \
 --data-path $DATA_PATH \
 --output-dir $OUTPUT_DIR \
