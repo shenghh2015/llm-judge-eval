9bccc3b88598fe0e22ab24cd5a3b56e6f597ff71
shenghua <shenghh2015@gmail.com>
2025-03-05 23:22:08 -0800

    modified:   utils/utils_sample/summarize.py

--- utils/utils_sample/summarize.py
+++ utils/utils_sample/summarize.py
@@ -99,9 +99,10 @@ def sample_data(data_path, num_splits, split_size):
                                  test_size=split_size,
                                  random_state=1234)
     _, test_index = next(iter(sss.split(absolute_indeces, preference_labels)))
-    print(len(test_index))
-    print(len(preference_labels[test_index]))
-    #print(preference_labels[test_index])
+    #print(len(test_index))
+    #print(len(preference_labels[test_index]))
+    print(text_index)
+    print(preference_labels[test_index])
     num_prefer_long = preference_labels[test_index].sum()
     
     num_prefer_not_long = len(test_index) - num_prefer_long
