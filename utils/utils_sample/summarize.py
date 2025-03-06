b8f558a8403b72405d786c2542b931697af40014
shenghua <shenghh2015@gmail.com>
2025-03-05 23:44:16 -0800

    modified:   utils/utils_sample/summarize.py

--- utils/utils_sample/summarize.py
+++ utils/utils_sample/summarize.py
@@ -97,7 +97,7 @@ def sample_data(data_path, num_splits, split_size):
     print(f"> split {split_idx}")
     sss = StratifiedShuffleSplit(n_splits=1,
                                  test_size=split_size,
-                                 random_state=1234)
+                                 random_state=123)
     #_, test_index = next(iter(sss.split(absolute_indeces, preference_labels)))
     _, test_index = next(sss.split(absolute_indeces, preference_labels))
     #print(len(test_index))
